35d9ffe5256a43f9d4f540101a913c2bf4f74e20
moizumi <mtcth210@gmail.com>
2020-11-08 18:15:27 +0900

    各サービスのバージョンを指定

--- provider.tf
+++ provider.tf
@@ -1,6 +1,11 @@
 provider "aws" {
-  version    = ">= 2.7"
-  region     = var.region
-  access_key = var.secret_access_key
-  secret_key = var.secret_key
+  version = "3.0"
+  region  = var.region
+  profile = var.aws_profile
+}
+provider "local" {
+  version = "2.0.0"
+}
+provider "tls" {
+  version = "3.0.0"
 }
